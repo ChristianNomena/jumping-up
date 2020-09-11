e6d78f9de650bac3d817e46668fb8ee92e815f3f
Ando <ando.andoniaina.nantenaina@esti.mg>
2020-09-11 13:32:56 +0300

    Ma version avec animations etc...

---
+++ scripts/hero.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+const GRAVITY = 1000
+const UP = Vector2(0, -1)
+const ACCEL = 50
+
+var velocity = Vector2()
+var max_speed = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	$AnimationPlayer.play("marche")
+	pass
+
+func _physics_process(delta):
+	velocity.x = 0
+	_process(delta)
+	velocity.y += GRAVITY * delta	
+	
+	move_and_slide(velocity, UP)
+	velocity = move_and_slide(velocity, UP)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):	
+	
+	var right = Input.is_action_pressed("ui_right")
+	var left = Input.is_action_pressed("ui_left")
+	var dirx = int(right) - int(left)
+	
+	if dirx == -1:
+		velocity.x = max(velocity.x - ACCEL, -max_speed)
+		$Sprite.flip_h = true
+		animation_loop("marche")
+	elif dirx == 1:
+		velocity.x = min(velocity.x + ACCEL, max_speed)
+		$Sprite.flip_h = false
+		animation_loop("marche")
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.15)
+		animation_loop("idle")
+		
+func animation_loop(animation):
+	if $AnimationPlayer.current_animation != animation:
+		$AnimationPlayer.play(animation)
